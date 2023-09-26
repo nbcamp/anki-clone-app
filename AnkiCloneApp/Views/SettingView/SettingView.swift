822193f4558c35fa23be7214401d8e27e4ed90eb
hidaehyunlee <hidaehyunlee@gmail.com>
2023-09-26 16:46:35 +0900

    Feat: TableView 생성

--- AnkiCloneApp/Views/SettingView/SettingView.swift
+++ AnkiCloneApp/Views/SettingView/SettingView.swift
@@ -2,19 +2,73 @@ import SnapKit
 import UIKit
 
 final class SettingView: UIView, RootView {
-    private lazy var label = {
-        let label = UILabel()
-        label.text = "Setting Screen"
-        label.sizeToFit()
-        return label
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.tableFooterView = UIView()
+        
+        return tableView
     }()
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initializeUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func initializeUI() {
         backgroundColor = .systemBackground
+        addSubview(tableView)
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
 
-        addSubview(label)
-        label.snp.makeConstraints { make in
-            make.center.equalTo(self)
+extension SettingView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "알림 관리"
+        case 1:
+            cell.textLabel?.text = "기본 리마인더 시간"
+        case 2:
+            cell.textLabel?.text = "앱 내 알림 표시"
+            let switchView = UISwitch()
+            cell.accessoryView = switchView
+        default:
+            break
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch indexPath.row {
+        case 0:
+            // Action Sheet
+            break
+        case 1:
+            // Time Picker로 시간 업데이트
+            break
+        default:
+            break
         }
     }
 }
